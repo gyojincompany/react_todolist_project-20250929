2d3c611afe474aacbb39134e315a69716077e104
gyojincompany <luckyred@gmail.com>
2025-09-29 12:36:54 +0900

    새로운 할일 리스트에 추가하기 완료

--- src/App.js
+++ src/App.js
@@ -2,13 +2,52 @@ import './App.css';
 import Header from './component/Header';
 import TodoEditor from './component/TodoEditor';
 import TodoList from './component/TodoList';
+import { useRef, useState } from 'react';
 
 function App() {
+  
+  const mockTodo = [
+    {
+      id: 0,
+      isDone: false,
+      content : "리액트 공부하기",
+      creatDate : new Date().getTime(),
+    },
+    {
+      id: 1,
+      isDone: false,
+      content : "빨래 널기",
+      creatDate : new Date().getTime(),
+    },
+    {
+      id: 2,
+      isDone: false,
+      content : "노래 연습하기",
+      creatDate : new Date().getTime(),
+    }
+  ];
+
+  const [todo, setTodo] = useState(mockTodo);
+
+  const idRef = useRef(3); //초깃값이 3인 ref객체 생성하여 idRef에 저장
+
+  function onCreate(content) { //추가 버튼이 클릭되면 실행될 이벤트 핸들러
+    const newItem = {
+      id: idRef.current, //idRef가 현재 저장하고 있는 값을 불러옴
+      content,//content:"밥먹기"
+      idDone : false,
+      creatDate : new Date().getTime()
+    };    
+    setTodo([newItem, ...todo]);//[...todo, newItem]->[{id0},{id1},{id2},{id3}]
+    //newItem->할일 객체를 생성한 후 idRef값을 1증가
+    idRef.current += 1;
+  }
+
   return (
     <div className="App">
       <Header />
-      <TodoEditor />
-      <TodoList />
+      <TodoEditor onCreate={onCreate} />
+      <TodoList todo={todo} />
     </div>
   );
 }
--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,15 +1,18 @@
 import "./TodoList.css"
 import TodoItem from "./TodoItem";
 
-function TodoList() {
+function TodoList({todo}) {
     return (
         <div className="TodoList">
             <h4>Todo List 🍚</h4>
             <input className="searchbar" placeholder="검색어를 입력하세요" />
             <div className="list_wrapper">
-                <TodoItem />
-                <TodoItem />
-                <TodoItem />
+      {/* todoItem을 3번 반복해서 출력 */}
+      {/* {id, isDone, content, creatDate}  */}
+      {/* {id, isDone, content, creatDate} -> props로 TodoItem 컴포넌트에 전달  */}
+                {todo.map((it) => (
+                    <TodoItem {...it} />                   
+                ))}
             </div>
         </div>
     );
