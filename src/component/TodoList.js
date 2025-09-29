d38be54276b59fa8b1b92d2fa80f68786f3c4fbd
gyojincompany <luckyred@gmail.com>
2025-09-29 10:49:12 +0900

    react_todolist_project-20250929

---
+++ src/component/TodoList.js
@@ -0,0 +1,18 @@
+import "./TodoList.css"
+import TodoItem from "./TodoItem";
+
+function TodoList() {
+    return (
+        <div className="TodoList">
+            <h4>Todo List 🍚</h4>
+            <input className="searchbar" placeholder="검색어를 입력하세요" />
+            <div className="list_wrapper">
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+            </div>
+        </div>
+    );
+}
+
+export default TodoList;
